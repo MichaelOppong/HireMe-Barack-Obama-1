c0ed2a31c8c03390924aed3dd4e79bb81b9e25ff
MichaelOppong <michael@scripted.org>
2020-10-08 10:45:11 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$("body").css("background-color", "blue");
+$("body").css("font-family", "'Roboto Condensed', sans-serif");
+$("h1").css("color", "white");
+$("h2").css("color", "red");
+$("a").css("color", "white");
+$(".about").css("font-family", "'Sansita Swashed', cursive");
+$(".likes").css("color", "white");
+$(".future").css("background-color", "red");
+$(".future").css("color", "white");
